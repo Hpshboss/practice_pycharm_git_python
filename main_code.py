332a151199a4ac0569a48a0690a1f1e597ed4fea
Hpshboss <s9303935522@gmail.com>
2019-04-03 23:32:08 +0800

    for loop if

--- main_code.py
+++ main_code.py
@@ -2,3 +2,5 @@ print("hello, this is my first git example.")
 print("because of some errors, this is trying again.")
 for index in range(10):
     print(index)
+    if index == 5:
+        print("this is five")
