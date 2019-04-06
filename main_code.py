591b7e811c600e4d7b6c0a909736d4bd1dd60da4
Hpshboss <s9303935522@gmail.com>
2019-04-07 01:09:30 +0800

    test cv2 basic function

--- main_code.py
+++ main_code.py
@@ -1,1 +1,9 @@
-print(__name__)
+import numpy as np
+import cv2 as cv
+
+img = cv.imread("temp.png", 0)
+cv.imshow("hello image", img)
+cv.namedWindow("hello image", cv.WINDOW_NORMAL)
+cv.waitKey(0)
+cv.destroyAllWindows()
+cv.imwrite("copy_temp.png", img)
