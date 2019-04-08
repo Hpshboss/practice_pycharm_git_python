e88c9119de304323298aff4d9918a7246e40e313
Hpshboss <s9303935522@gmail.com>
2019-04-08 23:21:31 +0800

    Measuring Performance with OpenCV

--- main_code.py
+++ main_code.py
@@ -4,6 +4,12 @@ import os
 import sys
 
 img = cv.imread("temp.png", cv.IMREAD_COLOR)
+tick1 = cv.getTickCount()
+for i in range(5, 49, 2):
+    img1 = cv.medianBlur(img, i)
+tick2 = cv.getTickCount()
+time = (tick2 - tick1)/cv.getTickFrequency()
+print(time)
 cv.imshow("hello image", img)
 print(len(img))
 px = img[0, 0]
