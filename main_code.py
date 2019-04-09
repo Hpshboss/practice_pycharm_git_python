a684d103c756c73c765c3110e7ee43d58aa8296b
Hpshboss <s9303935522@gmail.com>
2019-04-09 23:37:32 +0800

    opencv test from youtube(sentdex)

--- main_code.py
+++ main_code.py
@@ -1,24 +1,39 @@
 import numpy as np
-import cv2 as cv
-import os
-import sys
+import cv2
+import matplotlib.pyplot as plt
 
-img = cv.imread("temp.png", cv.IMREAD_COLOR)
-tick1 = cv.getTickCount()
-for i in range(5, 49, 2):
-    img1 = cv.medianBlur(img, i)
-tick2 = cv.getTickCount()
-time = (tick2 - tick1)/cv.getTickFrequency()
-print(time)
-cv.imshow("hello image", img)
-print(len(img))
-px = img[0, 0]
-print(px)
-cv.namedWindow("hello image", cv.WINDOW_NORMAL)
-cv.waitKey(0)
-cv.destroyAllWindows()
-cv.imwrite("copy_temp.png", img)
+# img = cv2.imread("temp.png", cv2.IMREAD_GRAYSCALE)
+# IMREAD_COLOR = 1
+# IMREAD_GRAYSCALED = 0(default)
+# IMREAD_UNCHANGED = -1
+#
+# cv2.imshow("image", img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+#
+# plt.imshow(img, cmap="gray", interpolation="bicubic")
+# plt.plot([50, 100], [80, 100], "c", linewidth=5)
+# plt.show()
+#
+# cv2.imwrite("copy_temp.png", img)
 
-array = [[1, 2], [3, 4]]
-print(array[1][0])
-print(len(array))
+cap = cv2.VideoCapture(1)
+
+while True:
+    print("check0")
+    ret, frame = cap.read()
+    cv2.imshow("frame", frame)
+    print("check1")
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
