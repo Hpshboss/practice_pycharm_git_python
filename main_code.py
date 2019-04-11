1dd1845088184d5dc7f79208ba0c26d9dc8f9bc0
Hpshboss <s9303935522@gmail.com>
2019-04-11 23:40:27 +0800

    use logi cam to practice show video logi take

--- main_code.py
+++ main_code.py
@@ -1,29 +1,15 @@
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
 
-# img = cv2.imread("temp.png", cv2.IMREAD_GRAYSCALE)
-# IMREAD_COLOR = 1
-# IMREAD_GRAYSCALED = 0(default)
-# IMREAD_UNCHANGED = -1
-#
-# cv2.imshow("image", img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-#
-# plt.imshow(img, cmap="gray", interpolation="bicubic")
-# plt.plot([50, 100], [80, 100], "c", linewidth=5)
-# plt.show()
-#
-# cv2.imwrite("copy_temp.png", img)
 
 cap = cv2.VideoCapture(1)
 
 while True:
-    print("check0")
     ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
     cv2.imshow("frame", frame)
-    print("check1")
+    cv2.imshow("gray", gray)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
