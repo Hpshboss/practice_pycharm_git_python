4c2c4c84fa19a1e6bbe25abbfee727b67f05f028
Hpshboss <s9303935522@gmail.com>
2019-04-12 00:04:47 +0800

    draw or write something on image

--- main_code.py
+++ main_code.py
@@ -2,24 +2,22 @@ import numpy as np
 import cv2
 
 
-cap = cv2.VideoCapture(1)
+img = cv2.imread("temp.png", cv2.IMREAD_COLOR)
 
-while True:
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+cv2.line(img, (0, 0), (150, 150), (255, 255, 255), 15)
+cv2.rectangle(img, (15, 25), (200, 150), (0, 0, 255), 5)
+cv2.circle(img, (100, 63), 55, (255, 0, 0), 5)
 
-    cv2.imshow("frame", frame)
-    cv2.imshow("gray", gray)
+pts = np.array([[10, 5], [20, 30], [70, 20], [50, 10]], np.int32)
+# pts = pts.reshape((-1, 1, 2))
+cv2.polylines(img, [pts], True, (0, 255, 255), 3)
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img, "OpenCV", (100, 350), font, 2, (200, 255, 255), 5, cv2.LINE_AA)
 
-cap.release()
+cv2.imshow("image", img)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
-
-
 
 
 
