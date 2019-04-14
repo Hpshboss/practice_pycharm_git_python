1b85b1e662ab53ced74863d9fef763ae26bfa12a
Hpshboss <s9303935522@gmail.com>
2019-04-14 23:31:05 +0800

    image operation

--- main_code.py
+++ main_code.py
@@ -3,21 +3,16 @@ import cv2
 
 
 img = cv2.imread("temp.png", cv2.IMREAD_COLOR)
+px = img[55, 55]
+print(px)
 
-cv2.line(img, (0, 0), (150, 150), (255, 255, 255), 15)
-cv2.rectangle(img, (15, 25), (200, 150), (0, 0, 255), 5)
-cv2.circle(img, (100, 63), 55, (255, 0, 0), 5)
+roi = img[100:150, 100:150]
+print(roi)
 
-pts = np.array([[10, 5], [20, 30], [70, 20], [50, 10]], np.int32)
-# pts = pts.reshape((-1, 1, 2))
-cv2.polylines(img, [pts], True, (0, 255, 255), 3)
+img[100:150, 100:150] = [255, 255, 255]
 
-font = cv2.FONT_HERSHEY_SIMPLEX
-cv2.putText(img, "OpenCV", (100, 350), font, 2, (200, 255, 255), 5, cv2.LINE_AA)
 
 cv2.imshow("image", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
 
