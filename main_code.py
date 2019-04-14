1ad7a4b3d955936a95e7dae3d1f70b84c7afa550
Hpshboss <s9303935522@gmail.com>
2019-04-15 00:54:44 +0800

    add two image in one picture

--- main_code.py
+++ main_code.py
@@ -1,18 +1,14 @@
-import numpy as np
 import cv2
 
 
-img = cv2.imread("temp.png", cv2.IMREAD_COLOR)
-px = img[55, 55]
-print(px)
+img1 = cv2.imread("apple_pencil.jpg")
+img2 = cv2.imread("apple_pencil_plus.jpg")
 
-roi = img[100:150, 100:150]
-print(roi)
+add = img1 + img2
 
-img[100:150, 100:150] = [255, 255, 255]
-
-
-cv2.imshow("image", img)
+cv2.imshow("add", add)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
 
