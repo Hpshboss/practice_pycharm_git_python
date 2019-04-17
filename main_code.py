f010feae1b22adc9bd92e3ff89980eb6461c7ddf
Hpshboss <s9303935522@gmail.com>
2019-04-18 00:40:00 +0800

    practice

--- main_code.py
+++ main_code.py
@@ -7,13 +7,20 @@ img2 = cv2.imread("apple_pencil_plus.jpg")
 # add = img1 + img2
 # add = cv2.addWeighted(img1, 0.5, img2, 0.5, 0)
 rows, cols, channels = img2.shape
-rol = img1[0:rows, 0:cols]
+roi = img1[0:rows, 0:cols]
 
 img2gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 ret, mask = cv2.threshold(img2gray, 220, 255, cv2.THRESH_BINARY_INV)
 
-cv2.imshow("mask", mask)
+mask_inv = cv2.bitwise_not(mask)
 
+img1_bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
+img2_fg = cv2.bitwise_and(img2, img2, mask=mask)
+
+dst = cv2.add(img1_bg, img2_fg)
+img1[0:rows, 0:cols] = dst
+
+cv2.imshow("res", img1)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
