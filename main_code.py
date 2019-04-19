93f311332bd780a65de848d4074b9b1a6d272c03
Hpshboss <s9303935522@gmail.com>
2019-04-19 23:21:32 +0800

    thresholding practice

--- main_code.py
+++ main_code.py
@@ -1,28 +1,18 @@
 import cv2
+import numpy as np
 
 
-img1 = cv2.imread("apple_pencil.jpg")
-img2 = cv2.imread("apple_pencil_plus.jpg")
+img = cv2.imread("bookpage.jpg")
+retval, threshold = cv2.threshold(img, 12, 255, cv2.THRESH_BINARY)
+grayscaled = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+retval2, threshold2 = cv2.threshold(grayscaled, 12, 255, cv2.THRESH_BINARY)
+gaus = cv2.adaptiveThreshold(grayscaled, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 155, 1)
 
-# add = img1 + img2
-# add = cv2.addWeighted(img1, 0.5, img2, 0.5, 0)
-rows, cols, channels = img2.shape
-roi = img1[0:rows, 0:cols]
-
-img2gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-ret, mask = cv2.threshold(img2gray, 220, 255, cv2.THRESH_BINARY_INV)
-
-mask_inv = cv2.bitwise_not(mask)
-
-img1_bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
-img2_fg = cv2.bitwise_and(img2, img2, mask=mask)
-
-dst = cv2.add(img1_bg, img2_fg)
-img1[0:rows, 0:cols] = dst
-
-cv2.imshow("res", img1)
+cv2.imshow("original", img)
+cv2.imshow("threshold", threshold)
+cv2.imshow("rthreshold2", threshold2)
+cv2.imshow("gaus", gaus)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
 
 
